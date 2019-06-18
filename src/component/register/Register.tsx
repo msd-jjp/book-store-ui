ab37ddc7d721150fe350a5346c7a5bf16b5b6672
hr.kazemi <hr.kazemi@gmail.com>
2019-06-18 19:20:03 +0430

    register updated

--- src/component/register/Register.tsx
+++ src/component/register/Register.tsx
@@ -7,13 +7,15 @@ enum REGISTER_STEP {
     register = 'register'
 }
 interface IState {
-    registerStep: REGISTER_STEP; //'submit_mobile' | 'validate_mobile' | 'register';
+    registerStep: REGISTER_STEP;
     mobile: {
         value: string | undefined;
         isValid: boolean;
     };
-    code: number | undefined;
-
+    code: {
+        value: string | undefined;
+        isValid: boolean;
+    };
     name: {
         value: string | undefined;
         isValid: boolean;
@@ -33,6 +35,8 @@ interface IState {
     isFormValid: boolean;
 }
 type TInputType = 'username' | 'password' | 'name' | 'code' | 'mobile' | 'confirmPassword';
+type TInputElType = 'inputElUsername' | 'inputElPassword' |
+    'inputElName' | 'inputElCode' | 'inputElMobile' | 'inputElConfirmPassword';
 
 class Register extends React.Component<any, IState> {
     props: any;
@@ -42,7 +46,10 @@ class Register extends React.Component<any, IState> {
             value: undefined,
             isValid: false,
         },
-        code: undefined,
+        code: {
+            value: undefined,
+            isValid: false,
+        },
         name: {
             value: undefined,
             isValid: false,
@@ -61,7 +68,16 @@ class Register extends React.Component<any, IState> {
         },
         isFormValid: false
     };
-    inputMobile!: HTMLInputElement;
+    inputElUsername!: HTMLInputElement;
+    inputElPassword!: HTMLInputElement;
+    inputElName!: HTMLInputElement;
+    inputElCode!: HTMLInputElement;
+    inputElMobile!: HTMLInputElement;
+    inputElConfirmPassword!: HTMLInputElement;
+
+    componentDidMount() {
+        this.focusOnInput('inputElMobile');
+    }
 
     gotoLogin() {
         this.props.history.push('/login');
@@ -98,17 +114,22 @@ class Register extends React.Component<any, IState> {
             throw new Error('should not happen !!!');
         }
     }
+
+    focusOnInput(inputEl: TInputElType): void {
+        this[inputEl].focus();
+    }
+
     submit_mobile_render() {
         if (this.state.registerStep === REGISTER_STEP.submit_mobile) {
             return (
                 <>
                     <Input
-                        defaultValue={this.state.username.value}
+                        defaultValue={this.state.mobile.value}
                         onChange={(val, isValid) => { this.handleInputChange(val, isValid, 'mobile') }}
                         label="mobile"
                         pattern={/^.{6,}$/}
                         patternError={'mobile format is not valid.'}
-                        elRef={input => { this.inputMobile = input; }}
+                        elRef={input => { this.inputElMobile = input; }}
                     />
 
                     <div className="form-group">
@@ -122,8 +143,66 @@ class Register extends React.Component<any, IState> {
         }
     }
     onSubmit_mobile() {
-        this.setState({ ...this.state, registerStep: REGISTER_STEP.validate_mobile });
+        this.setState(
+            {
+                ...this.state,
+                registerStep: REGISTER_STEP.validate_mobile,
+                isFormValid: false
+            },
+            () => this.focusOnInput('inputElCode')
+        );
     }
+    server_getCode() {
+        // check if counter is 0; 60--> 0
+        // send again else wait
+    }
+    validate_mobile_render() {
+        if (this.state.registerStep === REGISTER_STEP.validate_mobile) {
+            return (
+                <>
+                    <div>mobile: {this.state.mobile.value}
+                        <small className="text-info"
+                            onClick={() => this.from_validate_mobile_to_Submit_mobile()}
+                        >change mobile</small></div>
+
+                    <Input
+                        defaultValue={this.state.code.value}
+                        onChange={(val, isValid) => { this.handleInputChange(val, isValid, 'code') }}
+                        label="code"
+                        pattern={/^.{4,}$/}
+                        patternError={'code is not valid.'}
+                        elRef={input => { this.inputElCode = input; }}
+                    />
+
+                    <div className="form-group">
+                        <button className="btn btn-info mr-3"
+                            onClick={() => this.onValidate_mobile()}
+                            disabled={!this.state.isFormValid}
+                        >submit code</button>
+                    </div>
+
+
+                    <div>send again</div>
+                </>
+            )
+        }
+    }
+    onValidate_mobile() {
+        this.setState({ ...this.state, registerStep: REGISTER_STEP.register });
+    }
+    from_validate_mobile_to_Submit_mobile() {
+        this.setState(
+            {
+                ...this.state,
+                registerStep: REGISTER_STEP.submit_mobile,
+                isFormValid: true, // note: we go back to last step and it is valid.
+                code: { value: undefined, isValid: false }
+            },
+            () => this.focusOnInput('inputElMobile')
+        );
+    }
+
+
     render() {
         return (
             <>
@@ -135,13 +214,14 @@ class Register extends React.Component<any, IState> {
                         }
                         {
                             this.state.registerStep === REGISTER_STEP.validate_mobile
-                            && <div>validate mobile</div>
+                            && this.validate_mobile_render()
+                        }
+                        {
+                            this.state.registerStep === REGISTER_STEP.register
+                            && <div>register</div>
                         }
 
-
-                        <div>mobile --> submit</div>
-                        <div>code --> submit</div>
-                        <div>register -_> submit</div>
+                        <br /><br /><br />
                         <small className="text-info cursor-pointer" onClick={() => this.gotoLogin()}>login</small>
                     </div>
                 </div>
