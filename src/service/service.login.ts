298a81006a25dd91697bbf2da186a6056cc54db5
hr.kazemi <hr.kazemi@gmail.com>
2019-06-19 16:38:04 +0430

    service updated.

---
+++ src/service/service.login.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { IUser } from '../model/model.user';
+import { Setup } from '../config/setup';
+
+export class LoginService {
+
+    login(data: { username: string, password: string }): Promise<string> {
+        var token = data.username + ":" + data.password;
+        var hash = btoa(token);
+        let basic = "Basic " + hash;
+
+        const instance = axios.create({
+            baseURL: Setup.endpoint,
+            headers: { 'Content-Type': 'application/json', 'Authorization': basic }
+        });
+
+        return instance.post('/tokens', {});
+    }
+    profile(token: string/* , data: { username: string, password: string } */): Promise<IUser> {
+        const instance = axios.create({
+            baseURL: Setup.endpoint,
+            headers: { 'Content-Type': 'application/json', 'X-Custom-Header': token }
+        });
+
+        return instance.post('/profile', {});
+    }
+}
---
+++ src/service/service.register.ts
@@ -0,0 +1,49 @@
+import axios from 'axios';
+// import { IUser } from '../model/model.user';
+import { Setup } from '../config/setup';
+
+export class RegisterService {
+
+    sendCode(data: { cell_no: string }): Promise<any> {//{ cell_no: string; message: string; }
+        const instance = axios.create({
+            baseURL: Setup.endpoint,
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        return instance.post('/register/send-code', data);
+    }
+    activateAcount(data: {
+        "cell_no": string,
+        "activation_code": string
+    }): Promise<any> {//IUser
+        const instance = axios.create({
+            baseURL: Setup.endpoint,
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        return instance.post('/register/activate-acount', data);
+    }
+
+    signUp(data: {
+        "user": {
+            "password": string;
+            "username": string;
+        },
+        "persone": {
+            "address": string;
+            "email": string;
+            "last_name": string;
+            "name": string;
+            "phone": string;
+        },
+        "cell_no": string;
+        "signup_token": string;
+    }): any {
+        const instance = axios.create({
+            baseURL: Setup.endpoint,
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        return instance.post('/sign-up', data);
+    }
+}
