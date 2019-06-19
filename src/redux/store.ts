5b4abd3b7752c1dab62cf03ca77430873690b4b7
hr.kazemi <hr.kazemi@gmail.com>
2019-06-19 16:42:43 +0430

    user reducer, action interface updated.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { combineReducers, createStore, ReducersMapObject, AnyAction/* , Action */ } from 'redux';
 import { redux_state } from './app_state';
 import { reducer as UserReducer } from './reducer/user';
+import { IUser } from '../model/model.user';
+import { Reducer } from 'redux';
 
 const reducers: ReducersMapObject<redux_state, AnyAction> = { // Action
-    logged_in_user: UserReducer,
+    logged_in_user: UserReducer as Reducer<IUser | null, AnyAction>,
 }
 
 const main_reducer = combineReducers(reducers);
