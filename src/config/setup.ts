0aad16ecdfda47c2461bcef77f6ca09b9ae89af8
hr.kazemi <hr.kazemi@gmail.com>
2019-06-23 11:19:39 +0430

    regex & setup added.

--- src/config/setup.ts
+++ src/config/setup.ts
@@ -1,4 +1,46 @@
-export const Setup = {
+interface IInternationalization_fa {
+    rtl: true;
+    language: 'فارسی';
+    flag: 'fa';
+}
+interface IInternationalization_en {
+    rtl: false;
+    language: 'english';
+    flag: 'en';
+}
+
+interface ISetup {
+    endpoint: string;
+    documentTitle: string;
+    notify: {
+        timeout: {
+            error: number;
+            success: number;
+            warning: number;
+            info: number;
+            default: number;
+        };
+    };
+    recordDefaultLoadLength: 10;
+    internationalization: IInternationalization_en | IInternationalization_fa;
+}
+
+export const Setup: ISetup = {
     endpoint: '/api', // http://10.0.160.34:7000
-    documentTitle: 'Book Store'
+    documentTitle: 'Book Store',
+    notify: {
+        timeout: {
+            error: 4000,
+            success: 2500,
+            warning: 3000,
+            info: 3000,
+            default: 3000,
+        }
+    },
+    recordDefaultLoadLength: 10,
+    internationalization: {
+        rtl: false,
+        language: 'english',
+        flag: 'en',
+    }
 };
