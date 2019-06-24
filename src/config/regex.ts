367742dc5106f66477a08a08e008ba054677d9fe
hr.kazemi <hr.kazemi@gmail.com>
2019-06-24 15:57:33 +0430

    regex smsCode added

--- src/config/regex.ts
+++ src/config/regex.ts
@@ -11,5 +11,6 @@ export const AppRegex = {
     mobile: /^((\+|00)?\d{1,3})?[1-9][0-9]{9}$/,
     phone: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im,
     website: /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})/,
-    zipCode: ''
+    zipCode: '',
+    smsCode: /^\d{4}$/
 }
