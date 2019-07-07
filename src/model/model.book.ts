b4e630f4cebc87cd6170a7a9049125d2ae700203
hr.kazemi <hr.kazemi@gmail.com>
2019-07-07 12:27:41 +0430

    models added & updated

--- src/model/model.book.ts
+++ src/model/model.book.ts
@@ -1,5 +1,20 @@
 import { BaseModel } from "./model.base";
+import { BOOK_GENRE, BOOK_TYPES, BOOK_ROLES } from "../enum/Book";
+import { IPerson } from "./model.person";
 
 export interface IBook extends BaseModel {
     name: string;
+    edition: string;
+    genre: BOOK_GENRE[];
+    images: string[]; // image_url
+    language: string;
+    pub_year: string;
+    rate: number;
+    title: string;
+    type: BOOK_TYPES[]
+
+    roles: {
+        [key in BOOK_ROLES]: IPerson['id'];
+    };
+    files: string[]; // file-url
 }
---
+++ src/model/model.person.ts
@@ -0,0 +1,14 @@
+import { BaseModel } from "./model.base";
+import { IBook } from "./model.book";
+
+export interface IPerson extends BaseModel {
+    name: string;
+    last_name?: string;
+    address?: string;
+    phone: string;
+    image?: string;
+    email?: string;
+    cell_no?: string;
+    current_book_id?: string;
+    current_book?: IBook;
+}
