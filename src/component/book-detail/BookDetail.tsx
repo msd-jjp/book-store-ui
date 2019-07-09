e0ddd66034d11d5a98f42a025a7cceb94dc55e10
hr.kazemi <hr.kazemi@gmail.com>
2019-07-09 17:28:08 +0430

    book detail base started

--- src/component/book-detail/BookDetail.tsx
+++ src/component/book-detail/BookDetail.tsx
@@ -15,36 +15,71 @@ interface IProps {
 }
 interface IState {
   book: IBook | undefined;
+  pageLoader: boolean;
+  errorText: string | undefined;
 }
 class BookDetailComponent extends BaseComponent<IProps, IState> {
   state = {
-    book: undefined
+    book: undefined,
+    pageLoader: false,
+    errorText: undefined,
   };
   private _bookService = new BookService();
+  bookId!: string;
 
   componentDidMount() {
-    this.fetchBook(this.props.match.params.bookId);
+    this.bookId = this.props.match.params.bookId;
+    this.fetchBook(this.bookId);
   }
 
   async fetchBook(bookId: IBook["id"]) {
+    this.setState({ ...this.state, pageLoader: true });
+
     let res = await this._bookService.get(bookId).catch(error => {
-      this.handleError({ error: error.response });
+      const { body: errorText } = this.handleError({ error: error.response });
+
+      this.setState({ ...this.state, pageLoader: false, errorText });
     });
 
     if (res) {
-      this.setState({ ...this.state, book: res.data });
+      this.setState({ ...this.state, book: res.data, pageLoader: false, errorText: undefined });
     }
   }
-
+  book_detail_template(book: IBook) {
+    return (
+      <>
+        <div>{Localization.title}: {(this.state.book || { title: "" }).title}</div>
+      </>
+    )
+  }
+  book_render() {
+    if (this.state.book) {
+      return this.book_detail_template(this.state.book!);
+      
+    } else if (this.state.pageLoader) {
+      return (
+        <>
+          <div>{Localization.loading_with_dots}</div>
+        </>
+      )
+    } else if (this.state.errorText) {
+      return (
+        <>
+          <div>{this.state.errorText}</div>
+          <div onClick={() => this.fetchBook(this.bookId)}>
+            {Localization.retry}
+          </div>
+        </>
+      )
+    }
+  }
   render() {
     return (
       <>
         <div className="mt-5">
-          {!this.state.book && <div>error occured</div>}
 
-          {this.state.book && (
-            <div>{Localization.title}: {(this.state.book || { title: "" }).title}</div>
-          )}
+          {this.book_render()}
+
         </div>
 
         <ToastContainer {...this.getNotifyContainerConfig()} />
