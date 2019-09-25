619d3ecbd38d1fa623280ee3e7c1f868c50e639e
hr.kazemi <hr.kazemi@gmail.com>
2019-09-25 16:22:21 +0330

    reader worker type updated

--- src/webworker/reader-worker/reader.worker.ts
+++ src/webworker/reader-worker/reader.worker.ts
@@ -1,6 +1,10 @@
 import { ReaderUtils } from "./ReaderUtils";
 
-export default onmessage = async function (e) {
+interface IReaderMessageEvent extends MessageEvent {
+    data: { book_active_page: number };
+}
+
+export default onmessage = async function (e: IReaderMessageEvent) {
     // if (!e.data.book_id || !e.data.library) return;
     // if (!e.data.book_active_page) return;
     let bookSlideList = await ReaderUtils.getBookSlideList();
