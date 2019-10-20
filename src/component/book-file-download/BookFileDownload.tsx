2162e5c20fc366befc2de7c76f90127f2bddedaa
hr.kazemi <hr.kazemi@gmail.com>
2019-10-20 18:32:20 +0330

    cmp bookFileDownload added

--- src/component/book-file-download/BookFileDownload.tsx
+++ src/component/book-file-download/BookFileDownload.tsx
@@ -6,25 +6,17 @@ import { MapDispatchToProps, connect } from "react-redux";
 import { Dispatch } from "redux";
 import { BookService } from "../../service/service.book";
 import { ToastContainer } from "react-toastify";
-// import { IToken } from "../../model/model.token";
-// import { IUser } from "../../model/model.user";
-// import { action_user_logged_in } from "../../redux/action/user";
 import { NETWORK_STATUS } from "../../enum/NetworkStatus";
-// import { ICartItems, ICartItem } from "../../redux/action/cart/cartAction";
-// import { action_add_to_cart, action_remove_from_cart } from "../../redux/action/cart";
-// import { ILibrary_schema } from "../../redux/action/library/libraryAction";
+import { IDownloadingBookFile_schema } from "../../redux/action/downloading-book-file/downloadingBookFileAction";
+import { action_update_downloading_book_file, action_reset_downloading_book_file } from "../../redux/action/downloading-book-file";
+import { appLocalStorage } from "../../service/appLocalStorage";
 
 interface IProps {
     internationalization: TInternationalization;
-    // match: any;
-    // token: IToken;
-    // logged_in_user: IUser | null;
-    // onUserLoggedIn?: (user: IUser) => void;
     network_status: NETWORK_STATUS;
-    // cart: ICartItems;
-    // add_to_cart: (cartItem: ICartItem) => any;
-    // remove_from_cart: (cartItem: ICartItem) => any;
-    // library: ILibrary_schema;
+    downloading_book_file: IDownloadingBookFile_schema[];
+    update_downloading_book_file?: (data: IDownloadingBookFile_schema[]) => any;
+    reset_downloading_book_file?: () => any;
 }
 interface IState {
 
@@ -44,19 +36,53 @@ class BookFileDownloadComponent extends BaseComponent<IProps, IState> {
         // debugger;
         console.log('BookFileDownloadComponent componentDidMount');
         // if inprogress stop all of them. OR clear All of them --> clear all
+        this.props.reset_downloading_book_file!();
     }
 
     componentWillUnmount() {
         // debugger;
         console.log('BookFileDownloadComponent componentWillUnmount');
         // if inprogress stop all of them. (probebly clear all of them).
+        this.props.reset_downloading_book_file!();
     }
 
     componentWillReceiveProps(nextProps: IProps) {
-        debugger;
+        if (JSON.stringify(nextProps.downloading_book_file) !== JSON.stringify(this.props.downloading_book_file)) {
+            debugger;
+            let new_dbf = [...nextProps.downloading_book_file];
+
+            nextProps.downloading_book_file.forEach(d => {
+                if (d.status === 'start') {
+                    new_dbf = new_dbf.filter(nd => !(nd.book_id === d.book_id && nd.mainFile === d.mainFile));
+                    let new_d: IDownloadingBookFile_schema = { ...d, status: 'inProgress' };
+                    new_dbf.push(new_d);
+                    this.startDownload(d.book_id, d.mainFile);
+                }
+            });
+
+            this.props.update_downloading_book_file!(new_dbf);
+        }
+
         // check props.bookFileDownload, if changed:
         //  if found stop --> stop that request & remove it from redux
         //  if found start --> start downloading & replace it with inpropgress in redux
+    }
+
+
+    async startDownload(book_id: string, mainFile: boolean) {
+        let res = await this._bookService.downloadFile(book_id, mainFile).catch(e => { });
+
+        if (res) {
+            appLocalStorage.storeBookFile(book_id, mainFile, res.data);
+        }
+
+        this.downloadFinished(book_id);
+    }
+
+    downloadFinished(book_id: string) {
+        let dbf = [...this.props.downloading_book_file];
+        const existing_list = dbf.filter(d => d.book_id !== book_id);
+        this.props.update_downloading_book_file!(existing_list);
     }
 
     render() {
@@ -71,20 +97,16 @@ class BookFileDownloadComponent extends BaseComponent<IProps, IState> {
 
 const dispatch2props: MapDispatchToProps<{}, {}> = (dispatch: Dispatch) => {
     return {
-        // onUserLoggedIn: (user: IUser) => dispatch(action_user_logged_in(user)),
-        // add_to_cart: (cartItem: ICartItem) => dispatch(action_add_to_cart(cartItem)),
-        // remove_from_cart: (cartItem: ICartItem) => dispatch(action_remove_from_cart(cartItem)),
+        update_downloading_book_file: (data: IDownloadingBookFile_schema[]) => dispatch(action_update_downloading_book_file(data)),
+        reset_downloading_book_file: () => dispatch(action_reset_downloading_book_file()),
     };
 };
 
 const state2props = (state: redux_state) => {
     return {
         internationalization: state.internationalization,
-        // token: state.token,
-        // logged_in_user: state.logged_in_user,
         network_status: state.network_status,
-        // cart: state.cart,
-        // library: state.library,
+        downloading_book_file: state.downloading_book_file,
     };
 };
 
