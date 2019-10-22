e2dbaf185b6cc23757830278b89f1160a8944590
hr.kazemi <hr.kazemi@gmail.com>
2019-10-22 18:03:36 +0330

    reducer reader

--- src/redux/action/reader/readerAction.ts
+++ src/redux/action/reader/readerAction.ts
@@ -1,14 +1,18 @@
 import { Action } from "redux";
 import { EACTIONS } from "../../ActionEnum";
 
+type IReader_schema_epub_fontName = 'zar' | 'iransans' | 'nunito';
 export interface IReader_schema {
     audio: {};
     epub: {
         fontColor: number;
         bgColor: number;
         fontSize: number;
-        fontName: 'zar' | 'iransans' | 'nunito';
+        fontName: IReader_schema_epub_fontName;
         pageSize: { width: number, height: number };
+        fontFiles: {
+            [key in IReader_schema_epub_fontName]?: Array<number>;
+        }
     };
 }
 
